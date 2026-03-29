f2b319d34118c3fcd0df8fc8d6f1138e13920bf6
Rishab830 <wizafoundersoffice@gmail.com>
2026-03-29 14:20:58 +0530

    removed redundant ProjectLayout files

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-this")
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/legalease_db")
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://rishabrnair05_db_user:<db_password>@legalease.unjapn5.mongodb.net/?appName=LegalEase")
     UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", "uploads")
